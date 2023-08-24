ae3e84803adb19574dfc61fd16609388844986a0
Daniel McCarney <daniel@binaryparadox.net>
2023-08-24 15:33:31 -0400

    ci: deny warnings for rustdoc.
    This commit updates the CI config to also deny warnings from `rustdoc`.
    
    One finding related to a malformed HTML tag is fixed to ensure CI passes
    with this new config.

--- src/lib.rs
+++ src/lib.rs
@@ -353,7 +353,7 @@ impl DnType {
 pub enum DnValue {
     /// A string of characters from the T.61 character set
     TeletexString(Vec<u8>),
-    /// An ASCII string containing only A-Z, a-z, 0-9, '()+,-./:=? and <SPACE>
+    /// An ASCII string containing only A-Z, a-z, 0-9, '()+,-./:=? and `<SPACE>`
     PrintableString(String),
     /// A string encoded using UTF-32
     UniversalString(Vec<u8>),
