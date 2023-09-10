af5c42872b4767a357ca8671d59c23c75f1dc2a1
Daniel McCarney <daniel@binaryparadox.net>
2023-09-10 13:30:17 -0400

    ext: implement subject alternative name.
    This commit lifts the subject alternative name extension into the `ext`
    module.

--- src/ext.rs
+++ src/ext.rs
@@ -1,12 +1,12 @@
 use std::collections::BTreeMap;
+use std::net::IpAddr;
 
 use yasna::models::ObjectIdentifier;
 use yasna::{DERWriter, Tag};
 
-use crate::oid::OID_AUTHORITY_KEY_IDENTIFIER;
-use crate::Certificate;
-
+use crate::oid::{OID_AUTHORITY_KEY_IDENTIFIER, OID_SUBJECT_ALT_NAME};
 use crate::Error;
+use crate::{Certificate, SanType};
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub(crate) enum Criticality {
@@ -83,6 +83,44 @@ pub(crate) fn authority_key_identifier(cert: &Certificate) -> Extension {
 					.write_tagged_implicit(Tag::context(0), |writer| {
 						writer.write_bytes(cert.get_key_identifier().as_ref())
 					})
+			});
+		}),
+	}
+}
+
+/// An X.509v3 subject alternative name extension according to
+/// [RFC 5280 4.2.1.6](https://www.rfc-editor.org/rfc/rfc5280#section-4.2.1.6).
+pub(crate) fn subject_alternative_names(names: &Vec<SanType>) -> Extension {
+	Extension {
+		oid: ObjectIdentifier::from_slice(OID_SUBJECT_ALT_NAME),
+		// TODO(XXX): For now we mark the SAN extension as non-critical, matching the pre-existing
+		// 			  handling, however per 5280 this extension's criticality is determined based
+		//            on whether or not the subject contains an empty sequence. If it does, the
+		//            SAN MUST be critical. If it has a non-empty subject distinguished name,
+		//            the SAN SHOULD be non-critical.
+		criticality: Criticality::NonCritical,
+		der_value: yasna::construct_der(|writer| {
+			/*
+			   SubjectAltName ::= GeneralNames
+			   GeneralNames ::= SEQUENCE SIZE (1..MAX) OF GeneralName
+			*/
+			writer.write_sequence(|writer| {
+				for san in names {
+					writer.next().write_tagged_implicit(
+						Tag::context(san.tag()),
+						|writer| match san {
+							SanType::Rfc822Name(name)
+							| SanType::DnsName(name)
+							| SanType::URI(name) => writer.write_ia5_string(&name),
+							SanType::IpAddress(IpAddr::V4(addr)) => {
+								writer.write_bytes(&addr.octets())
+							},
+							SanType::IpAddress(IpAddr::V6(addr)) => {
+								writer.write_bytes(&addr.octets())
+							},
+						},
+					);
+				}
 			});
 		}),
 	}
