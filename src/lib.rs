add02c1e8d0f7475cfe7171fe83bb6d9bcb72301
Daniel McCarney <daniel@binaryparadox.net>
2023-09-10 14:49:39 -0400

    ext: implement custom extensions
    This commit lifts the custom extension handling into the `ext`
    module.

--- src/lib.rs
+++ src/lib.rs
@@ -916,16 +916,6 @@ impl CertificateParams {
 								for ext in extensions.iter() {
 									ext.write_der(writer.next());
 								}
-
-								// Write custom extensions
-								for ext in custom_extensions {
-									write_x509_extension(
-										writer.next(),
-										&ext.oid,
-										ext.critical,
-										|writer| writer.write_der(ext.content()),
-									);
-								}
 							});
 						});
 					});
@@ -987,13 +977,6 @@ impl CertificateParams {
 						//       extensions to self.extensions().
 						for ext in extensions.iter() {
 							ext.write_der(writer.next());
-						}
-
-						// Write the custom extensions
-						for ext in &self.custom_extensions {
-							write_x509_extension(writer.next(), &ext.oid, ext.critical, |writer| {
-								writer.write_der(ext.content())
-							});
 						}
 					});
 				});
@@ -1083,7 +1066,7 @@ impl CertificateParams {
 			exts.add_extension(ext::basic_constraints(&self.is_ca))?;
 		}
 
-		// TODO: custom extensions
+		exts.add_custom_extensions(&self.custom_extensions)?;
 
 		Ok(exts)
 	}
